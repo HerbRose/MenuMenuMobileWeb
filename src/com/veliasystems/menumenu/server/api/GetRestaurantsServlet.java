eff0136b090ea8e2f6d02ea2aab615920cd4d32e
Przemek Jakwert <pjakwert@gmail.com>
2012-09-28 13:19:55 +0200

    JSON API - getRestaurants servlet, test mock data added:
    Usage:
    
    http://127.0.0.1:8888/getRestaurants?token=a1b2c3&test&limit=50
    
    test: parameter required for test data generation
    limit: parameter optional (default=10), number of test restaurants to
    generate

--- src/com/veliasystems/menumenu/server/api/GetRestaurantsServlet.java
+++ src/com/veliasystems/menumenu/server/api/GetRestaurantsServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.veliasystems.menumenu.client.R;
+import com.veliasystems.menumenu.client.Util;
 import com.veliasystems.menumenu.client.entities.Restaurant;
 import com.veliasystems.menumenu.client.services.StoreService;
 import com.veliasystems.menumenu.server.StoreServiceImpl;
@@ -29,12 +30,23 @@ public class GetRestaurantsServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		String token = req.getParameter("token");
+		String test = req.getParameter("test");
+		boolean isTest = false;
+		if (test!=null) isTest = true;
 		
 		if (token==null || token.isEmpty() || !token.equalsIgnoreCase(R.TOKEN)) {
 			resp.getWriter().println("Wrong Token");
 			resp.flushBuffer();
 			return;
 		}
+		
+		
+		if (isTest) {
+			doTest(req,resp);
+			return;
+		}
+		
+		
 		
 		String cityIdString = req.getParameter("cityId");
 		Integer cityId = 0;
@@ -96,6 +108,80 @@ public class GetRestaurantsServlet extends HttpServlet {
 		}
 		resp.flushBuffer();
 		
+	}
+	
+	
+	private void doTest( HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json");
+		
+		String jsonp = req.getParameter("jsonp");
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		if(jsonp != null) {
+			resp.getWriter().print(jsonp + "(");
+		}
+		
+		String limit = req.getParameter("limit");
+		int n = 10;
+		if (limit!= null) {
+			try {
+				n = Integer.parseInt(limit);
+			} catch (NumberFormatException e) {
+				n = 10;
+			}
+		}
+		
+		List< Map<String,String> > attributes = new ArrayList< Map<String,String>>();
+		
+		String[] names = { "Pimiento", "Warsztat", "Babcia Malina", "Cosa Nostra", "Ptaszyl", "La Scandale", "El Torro", "Rio Grande", "ChinTin" };
+		String[] addresses = { "Ptasia 5", "Malinowa 45", "Borowkowa 2", "Zielona 55", "Lotnikow 4", "Debow 55" };
+		
+		String[] profileImages = { "http://www.shangrestaurant.com/wp-content/uploads/home-restaurant.jpg", 
+							"http://alltopmovies.net/wp-content/uploads/2009/06/restaurant.jpg",
+							"http://www.nuffy.net/pics/cool/bizarre-restaurants/restaurant_undersea.jpg",
+							"http://www.hoteles-silken.com/hotel-berlaymont-brussels/files/2010/09/Restaurant-LObjectif.jpg",
+							"http://s3.amazonaws.com/sfb111/story_xlimage_2010_08_R5362_Inwood_Restaurant_Grades.jpg",
+							"http://parisparfait.typepad.com/paris_parfait/images/2008/08/31/chinese_restaurant_2.jpg" };
+		
+		String[] menuImages = { "http://graphixshare.com/uploads/posts/2011-05-07/1304774534_kedsbf6ujfqpmv5.jpeg",
+								"http://www.mightysweet.com/mesohungry/wp-content/uploads/2010/08/02-Ukrainian-East-Village-Restaurant-menu-board.jpg",
+								"http://www.mightysweet.com/mesohungry/wp-content/uploads/2011/05/05-La-Norte%C3%B1a-Restaurant-menu.jpg",
+								"http://http://kookykitsch.com/Portals/0/productimages/2790_01c68.jpg",
+								"http://www.tannery.ie/assets/images/tasting.JPG",
+								"http://http://farm2.staticflickr.com/1329/665553384_6229ab7047_z.jpg?zz=1" };
+								
+		String[] logoImages = { "http://www.g-renderings.com/images/Logos/400X250/Corporate/BellavinoLogo.jpg",
+								"http://www.hayashi.com.au/logo.gif",
+								"http://www.freelanceforum.org/files/imagecache/screenshot_full/portfolio/Sheryl%20Nelson/1-ID-Zazu.gif",
+								"http://http://m.artician.com/pu/OUHFIQYZPWRDLF3Q2CFR4UYGTUPDH3V5.preview.png",
+								"http://http://eatmoredrinkmore.com/wp-content/uploads/2011/12/Toki-Underground-Logo.jpg",
+								"http://www.cityhindusnetwork.org.uk/wp-content/uploads/2011/04/CHN-Networking-Dinner-logo-v2.jpg" };
+		
+		
+		for (int i=0; i<n; i++) {
+			Map<String,String> map = new HashMap<String,String>();
+			map.put( "id", ""+ i);
+			map.put( "name", names[(int) Util.getRandom(names.length)]);
+			map.put( "city", "Krakow");
+			map.put( "district", "District " + i);
+			map.put( "address", addresses[(int) Util.getRandom(addresses.length)]);
+			map.put( "logoImage", logoImages[(int) Util.getRandom(logoImages.length)]);
+			map.put( "menuImage", menuImages[(int) Util.getRandom(menuImages.length)]);
+			map.put( "profileImage", profileImages[(int) Util.getRandom(profileImages.length)]);
+			map.put( "lat", "" + Util.getRandom(180));
+			map.put( "lng", "" + Util.getRandom(180));
+			
+			attributes.add(map);
+		}
+		
+		resp.getWriter().print(gson.toJson(attributes));
+		
+		if(jsonp != null) {
+			resp.getWriter().print(")");
+		}
+		resp.flushBuffer();
 	}
 	
 	
