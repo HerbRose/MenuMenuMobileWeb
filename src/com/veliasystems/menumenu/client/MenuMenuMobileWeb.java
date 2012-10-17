34de9083651b0c0faaa4eab58f8ca4f6b55bc951
mateuszkuznik <mateusz@velia-systems.com>
2012-10-17 17:16:28 +0200

    merge

--- src/com/veliasystems/menumenu/client/MenuMenuMobileWeb.java
+++ src/com/veliasystems/menumenu/client/MenuMenuMobileWeb.java
@@ -14,12 +14,15 @@ public class MenuMenuMobileWeb implements EntryPoint {
 	public void onModuleLoad() {
 	
 		String logged = Cookies.getCookie(R.LOGGED_IN);
-		if (logged!=null) loggedIn = true;
+		//System.out.println(loggedIn + logged);
+		if (logged != null){ loggedIn = true;}
 		
-		
+		//System.out.println(loggedIn);
 		if (loggedIn) {
-			JQMContext.changePage( new LoadDataScreen() );
+			//System.out.println("page nie login");
+			JQMContext.changePage( new LoadDataScreen(logged) );
 		} else {
+			//System.out.println("page login");
 			JQMContext.changePage( Pages.PAGE_LOGIN );
 		}
 	
