436d741652f6ac33217c70fae94d7f05e9904d22
mateuszdyrda <mateuszdyrda@10.0.0.206>
2013-03-04 15:51:57 +0100

    Add backup and restore task

--- src/com/veliasystems/menumenu/server/api/Restore.java
+++ src/com/veliasystems/menumenu/server/api/Restore.java
@@ -1,38 +1,20 @@
 package com.veliasystems.menumenu.server.api;
 
 
+import static com.google.appengine.api.taskqueue.TaskOptions.Builder.withUrl;
+
 import java.io.IOException;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import sun.util.resources.CalendarData;
-
-import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.googlecode.objectify.Query;
-import com.ibm.icu.util.Calendar;
-import com.sun.tools.javac.util.Convert;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
 import com.veliasystems.menumenu.client.R;
-import com.veliasystems.menumenu.client.entities.BackUpBlobKey;
-import com.veliasystems.menumenu.client.entities.City;
-import com.veliasystems.menumenu.client.entities.ImageBlob;
-import com.veliasystems.menumenu.client.entities.Restaurant;
-import com.veliasystems.menumenu.client.entities.User;
-import com.veliasystems.menumenu.client.services.StoreService;
-import com.veliasystems.menumenu.server.BlobServiceImpl;
 import com.veliasystems.menumenu.server.DAO;
 import com.veliasystems.menumenu.server.StoreServiceImpl;
 
@@ -55,8 +37,7 @@ public class Restore extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		
-		String token = req.getParameter("token");
+String token = req.getParameter("token");
 		
 		if (token==null || token.isEmpty() || !token.equalsIgnoreCase(R.TOKEN)) {
 			resp.getWriter().println("Wrong Token");
@@ -64,51 +45,27 @@ public class Restore extends HttpServlet {
 			return;
 		}
 		
-		BlobServiceImpl BSI = new BlobServiceImpl();
-//		
-//		resp.setCharacterEncoding("UTF-8");
-//		resp.setContentType("application/json");
-//		
-//		String jsonp = req.getParameter("jsonp");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json");
 		
-		if (getBackUpAddress()!=null||getLastListOfBUBK()!=null){BSI.restoreBackuoDB(getBackUpAddress(), getLastListOfBUBK());}
-			else resp.getWriter().print("Brak wpisu w bazie danych");
-
-//			
-//		if(jsonp != null) {
-//			resp.getWriter().print(")");
-//		}
-//		resp.flushBuffer();
-			
+		Queue queue = QueueFactory.getDefaultQueue();
+	    queue.add(withUrl("/tasks/RestoreTask").param("token", "a1b2c3"));
 		
-	}
-	private List<BackUpBlobKey> getLastListOfBUBK(){
-		Query <BackUpBlobKey>bubkQuery = dao.ofy().query(BackUpBlobKey.class);
-			List<BackUpBlobKey> BUBK = bubkQuery.order("-timeInMiliSec").list();
-		try{	
-		return BUBK;
-		}catch(IndexOutOfBoundsException e){
-			log.severe("Null");
-		}return null;
-	}
-	private List<String> getBackUpAddress(){
-		Query<BackUpBlobKey> query = dao.ofy().query(BackUpBlobKey.class);
-		List <String> getBackUpAddress = new ArrayList <String>();
 		
-		List <BackUpBlobKey> BUBK = query.order("-timeInMiliSec").list();
-		for (BackUpBlobKey backUp : BUBK) {
-			
-			getBackUpAddress.add(backUp.getAddress());
-		}
 		
-		try {
-		return getBackUpAddress;
-		}catch (IndexOutOfBoundsException e){
-			log.severe("Entie is null");
-		}catch (IllegalArgumentException ex){
-			log.severe("cos");
-		}
-		return null;
-	}	
+		String jsonp = req.getParameter("jsonp");
+		
+		
 	
+		if(jsonp != null) {
+			resp.getWriter().print(jsonp + "(");
+		}
+		resp.getWriter().print("Restore Complited");
+		if(jsonp != null) {
+			resp.getWriter().print(")");
+		}
+		resp.flushBuffer();
+	
+	
+}
 }
